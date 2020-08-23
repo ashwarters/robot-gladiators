42b026eb3754338d299ac801bd6c3c8a474062f9
Ashley Warters <ashwarters@gmail.com>
2020-08-23 10:50:00 -0400

    added if/else and window alert/end of lesson

---
+++ random.js
@@ -0,0 +1,8 @@
+for (var i = 0; i <= 10; i++) {
+    var count = 0;
+    var loopEnd = Math.floor(Math.random() * 10);
+    for (var j = 0; j < loopEnd {
+        count++;
+    }
+    console.log(count);
+}
